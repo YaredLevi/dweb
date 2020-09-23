1bbadc3c17a27c3d86b9bf82f3ac17a18430df76
marselo <ma.aburtog@alumnos.duoc.cl>
2020-09-23 04:08:06 -0300

    #006 añadido registro/inicio v2

--- src/index.py
+++ src/index.py
@@ -10,5 +10,9 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
